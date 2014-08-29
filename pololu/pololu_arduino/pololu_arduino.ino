6dfcf0387f166904d473d2fa9f35f8b330f3c9a6
nicrip <nick.rypkema@gmail.com>
2014-08-29 11:38:51 -0400

    updated pololu arduino code

--- pololu/pololu_arduino/pololu_arduino.ino
+++ pololu/pololu_arduino/pololu_arduino.ino
@@ -1,44 +1,80 @@
-//String inData;
-int incomingByte;
+String inData = "";
+String inDataSingle = "";
+unsigned int servo0, servo1, servo2, servo3, servo4, servo5;
+unsigned int count = 0;
 
-void setup()// run once, when the sketch starts
-{
-  Serial.begin(9600);   //serial for interface between arduino and lubuntu
-  //Serial1.begin(9600);  //serial for sending commands to pololu micro serial controller
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
 }
- 
-void loop() //Main program loop, executes forever after setup()
-{
-  //if (Serial.available() > 0) {
-  //  incomingByte = Serial.read();
-  //  Serial.print("received char: ");
-  //  Serial.println(incomingByte, DEC);
-    //inData += received;
-    //if (received == '\n') {
-    //  Serial.print("Arduino Received: ");
-    //  Serial.print(inData);
-    //  inData = "";
-    //}
-  //}
-  Serial.println("TEST");
-  put(7, 500);
-  delay(2000);
-  put(7,5500);
-  delay(2000);
+
+void loop() {
+  while (Serial.available() > 0)
+  {
+    char received = Serial.read();
+    inData += received; 
+
+    if (received == ',') {
+      Serial.print("Val Received: ");
+      Serial.println(inDataSingle);
+      setServo(inDataSingle);
+      inDataSingle = "";
+      count++;
+    } else {
+      inDataSingle += received;
+    }
+
+    // Process message when new line character is recieved
+    if (received == '\n') {
+      Serial.print("Final Val Received: ");
+      Serial.println(inDataSingle);
+      setServo(inDataSingle);
+      Serial.print("Line Received: ");
+      Serial.print(inData);
+      inData = ""; // Clear recieved buffer
+      inDataSingle = "";
+      count = 0;
+    }
+  }
 }
- 
-/*
- Move Servo
-*/
-void put(int servo, int angle)
-{
+
+void setServo(String angle) {
+  if (count == 0) {
+    servo0 = angle.toInt();
+    pushServo(0, servo0);
+  } else if (count == 1) {
+    servo1 = angle.toInt();
+    pushServo(1, servo1);
+  } else if (count == 2) {
+    servo2 = angle.toInt();
+    pushServo(2, servo2);
+  } else if (count == 3) {
+    servo3 = angle.toInt();
+    pushServo(3, servo3);
+  } else if (count == 4) {
+    servo4 = angle.toInt();
+    pushServo(4, servo4);
+  } else {
+    servo5 = angle.toInt();
+    pushServo(5, servo5);
+  }
+}
+
+void pushServo(unsigned int servo, unsigned int angle) {
   //servo is the servo number (typically 0-7)
   //angle is the absolute position from 500 to 5500
-  Serial1.write(0xAA);
-  Serial1.write(0x0C);
-  Serial1.write(0x04);
-  Serial1.write(servo);
-  //convert the angle data into two 7-bit bytes
-  Serial1.write(((angle>>7)&0x3f));
-  Serial1.write((angle&0x7f));
+  //Send a Pololu Protocol command
+  Serial1.write(0x80);               //start byte
+  Serial.print(0x80,HEX);
+  Serial1.write(0x01);               //device id
+  Serial.print(0x01,HEX);
+  Serial1.write(0x04);               //command number
+  Serial.print(0x04,HEX);
+  Serial1.write(servo);              //servo number
+  Serial.print(servo,DEC);
+  //Convert the angle data into two 7-bit bytes
+  Serial1.write(((angle>>7)&0x3f));  //data1
+  Serial.print(((angle>>7)&0x3f),HEX);
+  Serial1.write((angle&0x7f));       //data2
+  Serial.print((angle&0x7f),HEX);
 }
